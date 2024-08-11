bef27fe64a013c18ca03956dcb032662ec238608
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-08-11 23:03:01 +0530

    constant file index js added.

---
+++ src/constants/index.js
@@ -0,0 +1,610 @@
+import seo from '../assets/seomaster.png'
+import adminDashboard from "../assets/adminDashboard.png"
+import romiFinance from "../assets/romiFinance.png"
+import crypto from '../assets/cryptomarket.png'
+import fashion from '../assets/fashionhub.png'
+import calm from '../assets/calm.png'
+import medifine from '../assets/medifine.png'
+import justice from '../assets/justice.png'
+import creditcard from '../assets/creditcard.png'
+import gaming from '../assets/gaming.png'
+import realestate from '../assets/realestate.png'
+import beats from '../assets/beats.png'
+import dataanalytics from '../assets/dataanalytics.png'
+import vscode from '../assets/vscode.png'
+import fitfusion from '../assets/fitfusion.png'
+import videocraze from '../assets/videocraze.png'
+import todo from '../assets/todo.png'
+import githubuserfinder from '../assets/githubuserfinder.png'
+import calculator from '../assets/calculator.png'
+import tictactoe from '../assets/tictactoe.png'
+import html5 from '../assets/html5.png'
+import css3 from '../assets/css3.png'
+import tailwind from '../assets/tailwind.png'
+import javascript from '../assets/javascript.png'
+import react from '../assets/react.png'
+import git from '../assets/git.png'
+import github from '../assets/github.png'
+import www from '../assets/www.jpg'
+import cssselectors from '../assets/cssselectors.jpg'
+import cssboxmodel from '../assets/cssboxmodel.jpg'
+
+
+export const categoryNames = [
+  {
+    id: 'htmlcss',
+    title: 'HTML & CSS'
+  },
+	{
+		id: 'tailwindcss',
+    title: 'Tailwind CSS'
+	},
+  {
+    id: 'js',
+    title: 'JavaScript'
+  },
+  {
+    id: 'rjs',
+    title: 'React'
+  }
+];
+
+export const htmlCssProjects = [
+  {
+		name: "SEO Master HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: seo,
+		source_code_link: "https://github.com/vyomPundhir/SEO_Master_Homepage",
+		live_link: "https://seo-master-homepage.vercel.app/",
+	},
+  {
+		name: "Crypto Market HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: crypto,
+		source_code_link: "https://github.com/vyomPundhir/Crypto_Market_Homepage",
+		live_link: "https://crypto-market-homepage.vercel.app/",
+	},
+  {
+		name: "Fashion Hub HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: fashion,
+		source_code_link: "https://github.com/vyomPundhir/Fashion_Hub_Homepage",
+		live_link: "https://fashion-hub-homepage.vercel.app/",
+	},
+  {
+		name: "Calm HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: calm,
+		source_code_link: "https://github.com/vyomPundhir/Calm_Homepage",
+		live_link: "https://calm-homepage.vercel.app/",
+	},
+  {
+		name: "Medifine HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: medifine,
+		source_code_link: "https://github.com/vyomPundhir/Medifine_Homepage",
+		live_link: "https://medifine-homepage.vercel.app/",
+	},
+  {
+		name: "Justice HomePage",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: justice,
+		source_code_link: "https://github.com/vyomPundhir/Justice_Homepage",
+		live_link: "https://justice-homepage.vercel.app/",
+	},
+  {
+		name: "Credit Card Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: creditcard,
+		source_code_link: "https://github.com/vyomPundhir/Credit_Card_Landing_Page",
+		live_link: "https://credit-card-landing-page.vercel.app/",
+	},
+  {
+		name: "Gaming Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: gaming,
+		source_code_link: "https://github.com/vyomPundhir/Gaming-Landing-Page",
+		live_link: "https://gaming-landing-page-six.vercel.app/",
+	},
+  {
+		name: "Real Estate Desktop Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: realestate,
+		source_code_link: "https://github.com/vyomPundhir/Real-Estate-Desktop-Page",
+		live_link: "https://real-estate-desktop-page.vercel.app/",
+	},
+  {
+		name: "Beats Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: beats,
+		source_code_link: "https://github.com/vyomPundhir/Beats-Landing-Page",
+		live_link: "https://beats-landing-page-alpha.vercel.app/",
+	},
+  {
+		name: "Data Analytics Landing Page",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+		],
+		image: dataanalytics,
+		source_code_link: "https://github.com/vyomPundhir/Data-Analytics-Landing-Page",
+		live_link: "https://data-analytics-landing-page.vercel.app/",
+	},
+]
+
+export const tailwindProjects = [
+	{
+		name: "VScode HomePage UI Clone",
+		description:
+			"A Single Webpage which is made exactly same as the template image provided by the UI Designer using core HTML & CSS without any functionalities involved in it.",
+		tags: [
+			{
+				name: "html",
+				color: "#1eff00",
+			},
+			{
+				name: "css",
+				color: "#ff0098",
+			},
+			{
+				name: "tailwindcss",
+				color: "#00f6ff",
+			},
+		],
+		image: vscode,
+		source_code_link: "https://github.com/vyomPundhir/VScode_Homepage_UI_Clone",
+		live_link: "https://vscode-homepage-ui-clone.vercel.app/",
+	},
+]
+
+export const javascriptProjects =[
+
+]
+
+export const reactjsProjects = [
+	{
+		name: "Fit Fusion",
+		description:
+			"A Fitness & Gym Exercises Application where the user can Search for the Exercise Description related to a Target Muscle, Body Part, Equipments, etc.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "rapidAPI",
+				color: "#00f6ff",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: fitfusion,
+		source_code_link: "https://github.com/vyomPundhir/Fit_Fusion",
+		live_link: "https://fit-fusion-mk.netlify.app/",
+	},
+
+	{
+		name: "Video Craze",
+		description:
+			"A YouTube like Video Streaming Application where the user can Play the Video, Search for Videos, Filter Out Categories, Explore Channel Detail page, etc.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "rapidAPI",
+				color: "#00f6ff",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: videocraze,
+		source_code_link: "https://github.com/vyomPundhir/Video_Craze",
+		live_link: "https://video-craze-tj.netlify.app/",
+	},
+
+	{
+		name: "Admin Dashboard",
+		description:
+			"A Responsive React Admin Panel UI which contains the User, single user, add user section and the Product, single product, add product section.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "rapidAPI",
+				color: "#00f6ff",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: adminDashboard,
+		source_code_link: "https://github.com/vyomPundhir/Admin_Dashboard",
+		live_link: "https://admin-dashboard-vp.netlify.app/",
+	},
+
+	{
+		name: "TODO App",
+		description:
+			"A simple TODO App where the user can create a TODO, update it's status and delete it as well.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: todo,
+		source_code_link: "https://github.com/vyomPundhir/Todo_App_React",
+		live_link: "https://todo-app-react-zeta-sooty.vercel.app/",
+	},
+
+	{
+		name: "GitHub User Finder App",
+		description:
+			"A simple App where the user can fetch the details about anyone public Github details by providing the corresponding username.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: githubuserfinder,
+		source_code_link: "https://github.com/vyomPundhir/GitHub_User_Finder",
+		live_link: "https://git-hub-user-finder-hazel.vercel.app/",
+	},
+
+	{
+		name: "Calculator App",
+		description:
+			"A Simple Calculator App where the user can make the basic operations like Addition, Subtraction, Multiplication and Division, along with the clear button that clears the entries.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: calculator,
+		source_code_link: "https://github.com/vyomPundhir/Calculator_App_Assignment",
+		live_link: "https://calculator-app-assignment.vercel.app/",
+	},
+
+	{
+		name: "Tic-Tac-Toe App",
+		description:
+			"A Tic-Tac-Toe App where the users can play the tic-tac-toe game.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: tictactoe,
+		source_code_link: "https://github.com/vyomPundhir/Tic_Tac_Toe_App",
+		live_link: "https://tic-tac-toe-app-tan.vercel.app/",
+	},
+
+	{
+		name: "Romi Finance",
+		description:
+			"A User Interface of Romi Finance which is made exactly same as the reference Website provided to me, using React, Tailwind and applying routing.",
+		tags: [
+			{
+				name: "react",
+				color: "#1eff00",
+			},
+			{
+				name: "rapidAPI",
+				color: "#00f6ff",
+			},
+			{
+				name: "tailwindcss",
+				color: "#ff0098",
+			},
+			
+		],
+		image: romiFinance,
+		source_code_link: "https://github.com/vyomPundhir/Pixel_Bridges_Assessment",
+		live_link: "https://romi-finance-vp.netlify.app/",
+	},
+]
+
+export const techStackNames = [
+	{
+			image: html5,
+			content: "HTML 5",
+			color: "#EF966C",
+	},
+
+	{
+		image: css3,
+		content: "CSS 3",
+		color: "#5A98E6",
+	},
+
+	{
+		image: tailwind,
+		content: "Tailwind CSS",
+		color: "#31D4CB",
+	},
+
+	{
+		image: javascript,
+		content: "JavaScript",
+		color: "#ffe500",
+	},
+
+	{
+		image: react,
+		content: "React",
+		color: "#8584FF",
+	},
+
+	{
+		image: git,
+		content: "Git",
+		color: "#F74728",
+	},
+
+	{
+		image: github,
+		content: "GitHub",
+		color: "#7D7D7D",
+	},
+]
+
+export const Blogs = [
+	{
+		image: www,
+		name: "Introduction to Web and HTML",
+		description: "This Blog comprises of a detailed discussion of the Web and HTML.",
+		link: "https://vyompundhir.hashnode.dev/introduction-to-web-and-html",
+	},
+	{
+		image: cssselectors,
+		name: "CSS Selectors",
+		description: "This Blog comprises of a detailed discussion of the Selectors in CSS.",
+		link: "https://vyompundhir.hashnode.dev/css-selectors",
+	},
+	{
+		image: cssboxmodel,
+		name: "CSS Box Model",
+		description: "This Blog comprises of a detailed discussion of the CSS Box Model, i.e., Margin, Padding & Border.",
+		link: "https://vyompundhir.hashnode.dev/css-box-model-padding-margin-border",
+	}
+]
+
+
+// export const technologies = [
+// 	{
+// 		name: "HTML 5",
+// 		icon: html,
+// 	},
+// 	{
+// 		name: "CSS 3",
+// 		icon: css,
+// 	},
+// 	{
+// 		name: "Tailwind CSS",
+// 		icon: tailwind,
+// 	},
+// 	{
+// 		name: "JavaScript",
+// 		icon: javascript,
+// 	},
+// 	// {
+// 	// 	name: "TypeScript",
+// 	// 	icon: typescript,
+// 	// },
+// 	{
+// 		name: "React JS",
+// 		icon: reactjs,
+// 	},
+// 	// {
+// 	// 	name: "Redux Toolkit",
+// 	// 	icon: redux,
+// 	// },
+// 	{
+// 		name: "Zustand",
+// 		icon: zustand,
+// 	},
+
+// 	// {
+// 	// 	name: "MongoDB",
+// 	// 	icon: mongodb,
+// 	// },
+// 	// {
+// 	// 	name: "Express JS",
+// 	// 	icon: express,
+// 	// },
+// 	// {
+// 	// 	name: "Node JS",
+// 	// 	icon: nodejs,
+// 	// },
+// 	// {
+// 	// 	name: "Three JS",
+// 	// 	icon: threejs,
+// 	// },
+
+// 	{
+// 		name: "git",
+// 		icon: git,
+// 	},
+// 	// {
+// 	// 	name: "figma",
+// 	// 	icon: figma,
+// 	// },
+// 	// {
+// 	// 	name: "docker",
+// 	// 	icon: docker,
+// 	// },
+// ];
+
+// // export const blogs = [
+// // 	{
+// // 		name: "JavaScript Array & It's Methods",
+// // 		image: arrayimg,
+// // 		live_link:
+// // 			"https://yashpundhir.hashnode.dev/javascript-array-and-its-methods",
+// // 	},
+// // 	{
+// // 		name: "REST & SPREAD Operators in JavaScript",
+// // 		image: restimg,
+// // 		live_link:
+// // 			"https://yashpundhir.hashnode.dev/rest-spread-operators-in-javascript",
+// // 	},
+// // 	{
+// // 		name: "Getting Started with ExpressJS",
+// // 		image: expressimg,
+// // 		live_link:
+// // 			"https://yashpundhir.hashnode.dev/getting-started-with-expressjs",
+// // 	},
+// // ];
